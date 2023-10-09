76f813532c8cf6df13c94cc40514287b4945ea0f
Malcolm Barnard <malcolm.a.barnard@gmail.com>
2023-10-09 15:12:12 -0500

    fixing map code and figure issue

--- Maps.R
+++ Maps.R
@@ -18,9 +18,9 @@ us_map <- ggplot(data=state, aes(x=long, y=lat, group=group)) +
   geom_polygon(color = "black",fill="white") + 
   guides(fill=FALSE) + 
   geom_polygon(data=mississippi, fill="black")
-  theme(axis.title.x=element_blank(), axis.text.x=element_blank(), axis.ticks.x=element_blank(),
-        axis.title.y=element_blank(), axis.text.y=element_blank(), axis.ticks.y=element_blank()) #+ 
-  #coord_fixed(1.3)
+theme(axis.title.x=element_blank(), axis.text.x=element_blank(), axis.ticks.x=element_blank(),
+      axis.title.y=element_blank(), axis.text.y=element_blank(), axis.ticks.y=element_blank()) #+ 
+#coord_fixed(1.3)
 us_map
 ggsave(filename="USLower48Map.tiff",us_map,
        width=7.5,height=4,units="in")
@@ -35,7 +35,7 @@ ms_map <- ggplot(data=mississippi, mapping=aes(x=long, y=lat, group=group)) +
   geom_polygon(data=mississippi_county, fill=NA, color="white") + 
   geom_polygon(color="black", fill=NA) + 
   ggtitle('Location of Limnocorral Experiment') + 
-  geom_star(aes(y=34.428060,x=-89.391507),colour="black",fill="black",size=3,starshape=1) +
+  geom_star(aes(y=34.428060,x=-89.391507),colour="black",fill="black",size=7,starshape=1) +
   theme(axis.title.x=element_blank(), axis.text.x=element_blank(), axis.ticks.x=element_blank(),
         axis.title.y=element_blank(), axis.text.y=element_blank(), axis.ticks.y=element_blank())
 ms_map
